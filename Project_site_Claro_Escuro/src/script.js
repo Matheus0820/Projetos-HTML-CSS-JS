ace01244cd6ed1b95a32567514864fe2ac255a88
Matheus Ramos <matheus.ramos.703@ufrn.edu.br>
2023-06-10 19:54:31 -0300

    Novas Modificações no Project_site_claro_escuro

--- Project_site_Claro_Escuro/src/script.js
+++ Project_site_Claro_Escuro/src/script.js
@@ -1,16 +1,25 @@
 var fleg = true
 function action() {
-    var bod = document.getElementById("body");
     if (fleg == true) {
-        document.getElementById("butaoimg").innerHTML = "<img style=\"width: 100px;\" src=\"img/Buttao tema escuro.png\">";
-        bod.style.background = "black";
-        bod.style.color = "white";
-        fleg = false;
+        claro();
     } else {
-        document.getElementById("butaoimg").innerHTML = "<img style=\"width: 100px;\" src=\"img/Buttao tema claro.png\">";
-        bod.style.background = "white";
-        bod.style.color = "black";
-        fleg = true;
+        escuro();
     }
+}
+
+function claro() {
+    var bod = document.getElementById("body");
+    document.getElementById("butaoimg").innerHTML = "<img style=\"width: 100px;\" src=\"img/Buttao tema escuro2.png\">";
+    bod.style.background = "black";
+    bod.style.color = "white";
+    fleg = false;
+}
+
+function escuro() {
+    var bod = document.getElementById("body");
+    document.getElementById("butaoimg").innerHTML = "<img style=\"width: 100px;\" src=\"img/Buttao tema claro2.png\">";
+    bod.style.background = "white";
+    bod.style.color = "black";
+    fleg = true;
 }
        
